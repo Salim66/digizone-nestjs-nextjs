d42a4279feaa7d9dccb07aa36f21e6272f67f7f9
salim66 <salimhasanriad8@gmail.com>
2023-12-28 23:35:02 +0600

    initail setup done with mongodb database connection

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import config from 'config';
+
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(config.get('mongoDbUrl'), {
+      useNewUrlParser: true,
+      keepAlive: true,
+      useUnifiedTopology: true,
+      w: 1,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
